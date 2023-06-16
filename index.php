7ada0af1019824696622a7587a474d663c24b103
Andres <andrestacuaman13@gmail.com>
2023-06-16 11:30:42 -0500

    Estilos login

--- index.php
+++ index.php
@@ -10,13 +10,14 @@
     <link rel="stylesheet" href="plugins/sweetalert2/sweetalert2.min.css">  
     
     <link rel="stylesheet" type="text/css" href="css/fuentes/iconic/css/material-design-iconic-font.min.css">
+  
 
 </head>
 <body>
 <div class="container-login">
         <div class="wrap-login">
             <form class="login-form validate-form" id="formLogin" action="" method="post">
-                <span class="login-form-title">LOGIN</span>
+                <span class="login-form-title">Ingreso de Usuarios</span>
                 
                 <div class="wrap-input100" data-validate = "Usuario incorrecto">
                     <input class="input100" type="text" id="usuario" name="usuario" placeholder="Usuario">
@@ -37,5 +38,12 @@
             </form>
         </div>
     </div>   
+
+    <script src="jquery/jquery-3.3.1.min.js"></script>    
+    <script src="bootstrap/js/bootstrap.min.js"></script>    
+    <script src="popper/popper.min.js"></script>         
+    <script src="plugins/sweetalert2/sweetalert2.all.min.js"></script>    
+    <script src="codigo.js"></script> 
+
 </body>
 </html>
