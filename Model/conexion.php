b7a12a905bccd05bfad05b973b412021c734c7ec
Andres <andrestacuaman13@gmail.com>
2023-06-16 09:33:39 -0500

    Creación de la carpeta Model y conexion.php

---
+++ Model/conexion.php
@@ -0,0 +1,17 @@
+<?php
+class Conexion{
+    public function Conectar(){
+        define("servidor" , "localhost");
+        define("nombreBD" , "hatunsoft");
+        define("usuario" , "root");
+        define("password" , "");
+        $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8');
+        try {
+            $conexion = new PDO("mysql:host=".servidor."; dbname=".nombreBD,usuario,password, $opciones);
+            return $conexion;
+        } catch (Exception $e){
+            die("Error en la conexion: " . $e->getMessage());
+        }
+    } 
+}
+?>
